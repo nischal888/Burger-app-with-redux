1c7018ae4a8f93b83e2f91159d25734fe33c1857
nischal <nischalbudhathoki@nischals-MacBook-Air.local>
2020-10-19 18:57:50 +0545

    build file added

---
+++ build/precache-manifest.2a58c8cf665c870c46db074573922db5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a8f007fc2a0d5956901fa1f525b1ff",
+    "url": "/Burger-app-with-redux/index.html"
+  },
+  {
+    "revision": "af6eff7d97c0cd5cb19a",
+    "url": "/Burger-app-with-redux/static/css/main.498896b2.chunk.css"
+  },
+  {
+    "revision": "d65aeb6fc5ddb5cbbea3",
+    "url": "/Burger-app-with-redux/static/js/2.eeba2227.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Burger-app-with-redux/static/js/2.eeba2227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6eff7d97c0cd5cb19a",
+    "url": "/Burger-app-with-redux/static/js/main.78187008.chunk.js"
+  },
+  {
+    "revision": "215ad580c096ad9c655e",
+    "url": "/Burger-app-with-redux/static/js/runtime-main.0023f4b4.js"
+  },
+  {
+    "revision": "fbe040cec27b2c6517e69ecbd0647992",
+    "url": "/Burger-app-with-redux/static/media/burger.fbe040ce.svg"
+  }
+]);
