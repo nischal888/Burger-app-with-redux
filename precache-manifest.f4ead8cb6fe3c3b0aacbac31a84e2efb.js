678a7401aa96d121bedd5cb3b3cc2c8c285054b2
nischal budhathoki <nischalbudhathoki@nischals-MacBook-Air.local>
2020-10-19 17:44:04 +0545

    Updates

---
+++ precache-manifest.f4ead8cb6fe3c3b0aacbac31a84e2efb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdee0f39902df87afc2d731e2eb9d384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8608e9fd6f0450fc16a1",
+    "url": "/static/css/main.5e0198c6.chunk.css"
+  },
+  {
+    "revision": "b59578a53850bd9aa3a6",
+    "url": "/static/js/2.739642d3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.739642d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8608e9fd6f0450fc16a1",
+    "url": "/static/js/main.b394eec9.chunk.js"
+  },
+  {
+    "revision": "3c498821786cb422c55c",
+    "url": "/static/js/runtime-main.8d7ec1ce.js"
+  },
+  {
+    "revision": "fbe040cec27b2c6517e69ecbd0647992",
+    "url": "/static/media/burger.fbe040ce.svg"
+  }
+]);
